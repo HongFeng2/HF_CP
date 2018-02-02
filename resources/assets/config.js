636ce29ec95537452b3ce7720af07b7e36833089
完美风暴 <66964996@qq.com>
2018-02-02 17:19:31 +0800

    2018-2-2
    修改完成

---
+++ resources/assets/config.js
@@ -0,0 +1,7 @@
+var conf = {};
+conf.server = "http://localhost:844";
+// window.addEventListener('touchstart', function (event) {
+//     if(event.touches.length>1){
+//         event.preventDefault();
+//     }
+// });
